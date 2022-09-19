1f5adc4f9941c67c2dfe438bb4aed0674fcd11a2
dmitryame <dmitry@custommode.com>
2022-09-19 15:16:16 -0400

    wip

--- lambda-fns/index.ts
+++ lambda-fns/index.ts
@@ -1,13 +1,11 @@
 // ******************************************************
 //                       queries
 // ******************************************************
-import generateUploadUrl from './controllers/places/generateUploadUrl'
-
+import generateUploadUrl from "./controllers/places/generateUploadUrl"
 
 // ******************************************************
 //                       mutations
 // ******************************************************
-
 
 // import AbuseReport from './models/abuseReport'
 // import Photo from './models/photo'
@@ -16,48 +14,46 @@ import generateUploadUrl from './controllers/places/generateUploadUrl'
 type AppSyncEvent = {
   info: {
     fieldName: string
-  },
+  }
   arguments: {
     // photo: Photo,
     // abuseReport: AbuseReport,
-    placeId: string,
-    uuid: string,
-    lat: number,
-    lon: number,
-    pageNumber: number,
-    searchTerm: string,
-    description: string,
-    commentId: bigint,
-    video: boolean,
-    assetKey: string,
-    contentType: string,
-    nickName: string,
-    secret: string,
-    newSecret: string,
-    friendshipUuid: string,
-    invitedByUuid: string,
-    chatUuid: string,
-    messageUuid: string,
-    text: string,
-
+    placeId: string
+    uuid: string
+    lat: number
+    lon: number
+    pageNumber: number
+    searchTerm: string
+    description: string
+    commentId: bigint
+    video: boolean
+    assetKey: string
+    contentType: string
+    nickName: string
+    secret: string
+    newSecret: string
+    friendshipUuid: string
+    invitedByUuid: string
+    chatUuid: string
+    messageUuid: string
+    text: string
   }
 }
 
-exports.handler = async (event:AppSyncEvent) => {
+exports.handler = async (event: AppSyncEvent) => {
   switch (event.info.fieldName) {
     // ******************************************************
     //                       queries
     // ******************************************************
 
-    case 'generateUploadUrl':
+    case "generateUploadUrl":
       return await generateUploadUrl(
         event.arguments.assetKey,
-        event. arguments.contentType,
-      )    
-      // ******************************************************
-      //                       mutations
-      // ******************************************************
-
+        event.arguments.contentType
+      )
+    // ******************************************************
+    //                       mutations
+    // ******************************************************
 
     default:
       return null
