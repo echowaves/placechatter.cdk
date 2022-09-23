b5d7a564aa99cd36cb5f2763d3098b5cea72649b
dmitryame <dmitry@custommode.com>
2022-09-23 18:34:04 -0400

    added twillio

--- lambda-fns/controllers/phones/generateActivationCode.ts
+++ lambda-fns/controllers/phones/generateActivationCode.ts
@@ -37,17 +37,30 @@ export default async function main(uuid: string, phoneNumber: string) {
   await psql.clean()
 
   // send sms to a phoneNumber here
-  var params = {
-    PhoneNumber: `+1${phoneNumber}`,
-    Message: `Activation Code: ${smsCode}`,
-    MessageAttributes: {
-      'AWS.SNS.SMS.SMSType': {
-        DataType: 'String',
-        StringValue: 'Transactional',
-      },
-    },
-  }
-  await SNS.publish(params)
+  // var params = {
+  //   PhoneNumber: `+1${phoneNumber}`,
+  //   Message: `Activation Code: ${smsCode}`,
+  //   MessageAttributes: {
+  //     'AWS.SNS.SMS.SMSType': {
+  //       DataType: 'String',
+  //       StringValue: 'Transactional',
+  //     },
+  //   },
+  // }
+  // const publish_resp = await SNS.publish(params).promise()
+  // console.log({ publish_resp })
+
+  const accountSid = process.env.TWILIO_ACCOUNT_SID
+  const authToken = process.env.TWILIO_AUTH_TOKEN
+  const client = require('twilio')(accountSid, authToken)
+
+  const message = await client.messages.create({
+    body: `Activation Code: ${smsCode}`,
+    from: '+19303365867',
+    to: `+1${phoneNumber}`,
+  })
+
+  console.log({ message })
 
   return smsCode // 4 alpha numeric
 }
