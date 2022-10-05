836b992af670c4031e909db0ba1ac7058c45a876
dmitryame <dmitry@custommode.com>
2022-10-04 23:06:23 -0400

    wip: placeDetails

--- lambda-fns/controllers/places/placeDetails.ts
+++ lambda-fns/controllers/places/placeDetails.ts
@@ -30,8 +30,16 @@ export default async function main(
                     `)
   ).rows[0]
   console.log({ place })
+  const placeOwner = (
+    await psql.query(`
+                    SELECT * from "PlaceOwners"
+                    WHERE
+                    placeUuid = '${placeUuid}' 
+                    `)
+  ).rows[0]
+  console.log({ place, placeOwner })
 
   await psql.clean()
 
-  return place
+  return { place, placeOwner }
 }
