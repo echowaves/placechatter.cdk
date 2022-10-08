8b0fa2ec04f103893410a0ce272ced5b25b239c4
dmitryame <dmitry@custommode.com>
2022-10-08 19:21:39 -0400

    wip: create photo

--- lambda-fns/lambdas/processDeletedImage/index.ts
+++ lambda-fns/lambdas/processDeletedImage/index.ts
@@ -7,38 +7,45 @@ export async function main(event: any = {}, context: any) {
   const record = event.Records[0]
   const name = record.s3.object.key
 
-  const photoId = name.replace('-thumb', '')
+  const photoUuid = name.replace('-thumb', '')
   // we only want to deal with originals
   // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!deleting image: ${name}`)
   // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!       photoId: ${photoId}`)
 
   await Promise.all([
-    _deleteUpload({Bucket: record.s3.bucket.name, Key: `${photoId}`,}),
-    _cleanupTables({photoId,}),
+    _deleteUpload({ Bucket: record.s3.bucket.name, Key: `${photoUuid}` }),
+    _cleanupTables({ photoUuid }),
   ])
   // console.log('everything ended')
   // cb(null, 'success everything')
   return true
 }
 
-const _deleteUpload = async({Bucket, Key,}: {Bucket: string, Key: string}) => {
+const _deleteUpload = async ({
+  Bucket,
+  Key,
+}: {
+  Bucket: string
+  Key: string
+}) => {
   // console.log(`_deleteUpload started: ${Key}`)
 
   try {
     const s3 = new AWS.S3()
-    await s3.deleteObject({
-      Bucket,
-      Key,
-    }).promise()
+    await s3
+      .deleteObject({
+        Bucket,
+        Key,
+      })
+      .promise()
   } catch (err) {
     console.log('Error deleting object')
-    console.log({err,})
+    console.log({ err })
   }
   // console.log(`_deleteUpload ended: ${Key}`)
-
 }
 
-const _cleanupTables = async({photoId,}: {photoId: string}) => {
+const _cleanupTables = async ({ photoUuid }: { photoUuid: string }) => {
   // console.log(`_cleanupTables started: ${photoId}`)
 
   await psql.connect()
@@ -46,45 +53,28 @@ const _cleanupTables = async({photoId,}: {photoId: string}) => {
     await psql.query(`
                     DELETE from "Photos"
                     WHERE
-                    id = ${photoId}
-                    `
-    )
+                    "photoUuid" = ${photoUuid}
+                    `)
     //
   } catch (err) {
     console.log('Error de-activating photo')
-    console.log({err,})
+    console.log({ err })
   }
   // console.log(`_cleanupTables ended 1: ${photoId}`)
 
   try {
     await psql.query(`
-        DELETE from "Watchers"
+        DELETE from "PlacesPhotos"
                     WHERE
-                    "photoId" = ${photoId}
-                    `
-    )
+                    "photoUuid" = ${photoUuid}
+                    `)
     //
   } catch (err) {
-    console.log('Error cleaning up Watchers')
-    console.log({err,})
+    console.log('Error cleaning up PlacesPhotos')
+    console.log({ err })
   }
   // console.log(`_cleanupTables ended 2: ${photoId}`)
 
-  try {
-    await psql.query(`
-                    DELETE from "Recognitions"
-                    WHERE
-                    "photoId" = ${photoId}
-                    `
-    )
-    //
-  } catch (err) {
-    console.log('Error cleaning up Recognitions')
-    console.log({err,})
-  }
-  // console.log(`_cleanupTables ended 3: ${photoId}`)
-
   await psql.clean()
   // console.log(`_cleanupTables ended: ${photoId}`)
-
 }
--- lambda-fns/lambdas/processUploadedImage/index.ts
+++ lambda-fns/lambdas/processUploadedImage/index.ts
@@ -1,10 +1,10 @@
-import * as moment from 'moment'
-
 import psql from '../../psql'
+import { VALID } from '../../valid'
 
 const AWS = require('aws-sdk')
 
 const sharp = require('sharp')
+import * as dayjs from 'dayjs'
 
 // eslint-disable-next-line import/prefer-default-export
 export async function main(event: any = {}, context: any) {
@@ -15,7 +15,7 @@ export async function main(event: any = {}, context: any) {
   //
   const record = event.Records[0]
   const name = record.s3.object.key
-  const photoId = name.replace('.upload', '')
+  const photoUuid = name.replace('.upload', '')
   const Bucket = record.s3.bucket.name
   // we only want to deal with originals
   // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!received image: ${name}`)
@@ -24,24 +24,24 @@ export async function main(event: any = {}, context: any) {
   const s3 = new AWS.S3()
   // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!   ended 1    photoId: ${photoId}`)
 
-  let image =
-    await s3.getObject({
+  let image = await s3
+    .getObject({
       Bucket,
       Key: name,
-    }).promise()
-    // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!   ended 2    photoId: ${photoId}`)
+    })
+    .promise()
+  // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!   ended 2    photoId: ${photoId}`)
 
   await Promise.all([
-    _genWebpThumb({image, Bucket, Key: `${photoId}-thumb`,}),
-    _genWebp({image, Bucket, Key: `${photoId}`,}),
-    _recognizeImage({Bucket, Key: `${name}`,}),
+    _genWebpThumb({ image, Bucket, Key: `${photoUuid}-thumb` }),
+    _genWebp({ image, Bucket, Key: `${photoUuid}` }),
   ])
 
   // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!   ended 3    photoId: ${photoId}`)
 
   await Promise.all([
-    _deleteUpload({Bucket, Key: name,}),
-    _activatePhoto({photoId,}),
+    _deleteUpload({ Bucket, Key: name }),
+    _activatePhoto({ photoUuid }),
   ])
 
   // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!   ended 4   photoId: ${photoId}`)
@@ -50,159 +50,102 @@ export async function main(event: any = {}, context: any) {
   return true
 }
 
-const _genWebpThumb = async({image, Bucket, Key,}: {image: any, Bucket: string, Key: string}) => {
-// console.log(`_genWebpThumb started  ${Key}`)
-  const buffer = await sharp(image.Body).rotate().webp({lossless: false, quality: 80,}).resize({height: 300,}).toBuffer()
+const _genWebpThumb = async ({
+  image,
+  Bucket,
+  Key,
+}: {
+  image: any
+  Bucket: string
+  Key: string
+}) => {
+  // console.log(`_genWebpThumb started  ${Key}`)
+  const buffer = await sharp(image.Body)
+    .rotate()
+    .webp({ lossless: false, quality: 80 })
+    .resize({ height: 300 })
+    .toBuffer()
   const s3 = new AWS.S3()
-  await s3.putObject({
-    Bucket,
-    Key,
-    Body: buffer,
-    ContentType: 'image/webp',
-    ACL: 'public-read',
-    CacheControl: 'max-age=31536000',
-  }).promise()
+  await s3
+    .putObject({
+      Bucket,
+      Key,
+      Body: buffer,
+      ContentType: 'image/webp',
+      ACL: 'public-read',
+      CacheControl: 'max-age=31536000',
+    })
+    .promise()
 
   // console.log(`_genWebpThumb ended  ${Key}`)
 }
 
-const _genWebp = async({image, Bucket, Key,}: {image: any, Bucket: string, Key: string}) => {
+const _genWebp = async ({
+  image,
+  Bucket,
+  Key,
+}: {
+  image: any
+  Bucket: string
+  Key: string
+}) => {
   // console.log(`_genWebp started  ${Key}`)
 
-  const buffer = await sharp(image.Body).rotate().webp({lossless: false, quality: 90,}).toBuffer()
+  const buffer = await sharp(image.Body)
+    .rotate()
+    .webp({ lossless: false, quality: 90 })
+    .toBuffer()
   const s3 = new AWS.S3()
-  await s3.putObject({
-    Bucket,
-    Key,
-    Body: buffer,
-    ContentType: 'image/webp',
-    ACL: 'public-read',
-    CacheControl: 'max-age=31536000',
-  }).promise()
+  await s3
+    .putObject({
+      Bucket,
+      Key,
+      Body: buffer,
+      ContentType: 'image/webp',
+      ACL: 'public-read',
+      CacheControl: 'max-age=31536000',
+    })
+    .promise()
   // console.log(`_genWebp ended  ${Key}`)
 }
 
-const _deleteUpload = async({Bucket, Key,}: {Bucket: string, Key: string}) => {
+const _deleteUpload = async ({
+  Bucket,
+  Key,
+}: {
+  Bucket: string
+  Key: string
+}) => {
   // console.log(`_deleteUpload started  ${Key}`)
 
   const s3 = new AWS.S3()
-  await s3.deleteObject({
-    Bucket,
-    Key,
-  }).promise()
+  await s3
+    .deleteObject({
+      Bucket,
+      Key,
+    })
+    .promise()
   // console.log(`_deleteUpload ended  ${Key}`)
 }
 
-const _recognizeImage = async({Bucket, Key,}: {Bucket: string, Key: string}) => {
-  // console.log(`_recognizeImage started  ${Key}`)
-
-  // console.log({Bucket, Key})
-  const rekognition = new AWS.Rekognition()
-  const params = {
-    Image: {
-      S3Object: {
-        Bucket,
-        Name: Key,
-      },
-    },
-  }
-  // console.log(`_recognizeImage ended 1  ${Key}`)
-
-  const metaData = {
-    Labels: null,
-    TextDetections: null,
-    ModerationLabels: null,
-  }
-  try {
-    const [
-      labelsData,
-      moderationData,
-      textData,
-    ] =
-      await Promise.all([
-        rekognition.detectLabels(params).promise(),
-        rekognition.detectModerationLabels(params).promise(),
-        rekognition.detectText(params).promise(),
-      ])
-
-      // console.log(`_recognizeImage ended 2  ${Key}`)
-
-    metaData.Labels = labelsData.Labels
-    // console.log(JSON.stringify(labelsData))
-
-    metaData.ModerationLabels = moderationData.ModerationLabels
-    // console.log(JSON.stringify(moderationData))
-
-    metaData.TextDetections = textData.TextDetections
-    // console.log(JSON.stringify(textData))
-
-    // console.log(JSON.stringify(metaData))
-  } catch (err) {
-    console.log('Error parsing image')
-    console.log({err,})
-  }
-  // console.log(`_recognizeImage ended 3  ${Key}`)
-
-  try {
-    const createdAt = moment().format("YYYY-MM-DD HH:mm:ss.SSS")
-
-    // console.log({metaData: JSON.stringify(metaData),})
-    await psql.connect()
-    // const result =
-    // (
-    await psql.query(`    
-                  insert into "Recognitions"
-                    (
-                        "photoId",
-                        "metaData",
-                        "createdAt",
-                        "updatedAt"
-                    ) values (
-                      $1,
-                      $2,
-                      $3,
-                      $4
-                    )
-                    returning *
-                    `,
-    [
-      Key.replace('.upload', ''),
-      metaData,
-      createdAt,
-      createdAt,
-    ])
-    // ).rows
-    // console.log({result})
-  } catch (err) {
-    console.log('Error saving recognitions')
-    console.log({err,})
-  }
-  await psql.clean()
-  // console.log(`_recognizeImage ended 4  ${Key}`)
-
-}
-
-
-const _activatePhoto = async({photoId,}: {photoId: string}) => {
+const _activatePhoto = async ({ photoUuid }: { photoUuid: string }) => {
   // console.log(`_activatePhoto started  ${photoId}`)
 
   try {
-    const updatedAt = moment().format("YYYY-MM-DD HH:mm:ss.SSS")
+    const updatedAt = dayjs().format(VALID.dateFormat) // display
 
     await psql.connect()
     await psql.query(`    
                     UPDATE "Photos"
                     set active = true, "updatedAt" = '${updatedAt}'
                     WHERE
-                    id = ${photoId}
+                    "photoUuid" = ${photoUuid}
                     `)
     // console.log({result})
   } catch (err) {
     console.log('Error activating photo')
-    console.log({err,})
+    console.log({ err })
   }
   await psql.clean()
   // console.log(`_activatePhoto ended  ${photoId}`)
-
 }
-
