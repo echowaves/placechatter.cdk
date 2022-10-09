6a67119d42ebcd5e525d60142c8291e958b382e9
dmitryame <dmitry@custommode.com>
2022-10-08 21:33:00 -0400

    wip: create photo

--- lambda-fns/controllers/photos/generateUploadUrl.ts
+++ lambda-fns/controllers/photos/generateUploadUrl.ts
@@ -50,7 +50,7 @@ export default async function main(
                         "photoUuid",
                         "phoneNumber",
                         "active",
-                        "createdAt",
+                        "createdAt"
                     ) values (
                       '${photoUuid}',
                       '${phoneNumber}',
